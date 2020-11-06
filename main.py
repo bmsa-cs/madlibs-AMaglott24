4ccd332daaf892fab36f068b4f124154383ad76c
Alexander Maglott <amaglott24@biomedscienceacademy.org>
2020-11-06 18:00:26 +0000

    First half of the story is done, and 5 variables added.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Alex Maglott
+Period/Core: 6
 
 
 """
+print("Madlib: Forest Hike\n")
+name = input("Select a name: ")
+adjective1 = input("Select an adjective: ")
+weather = input("Select a type of weather: ")
+animal1 = input("Select an animal (Plural): ")
+adjective2 = input("Select an adjective: ")
+
+print("\n" + name + " was taking a " + adjective1 + " stroll through the forest.")
+print("The day was quite " + weather + "y, so it was perfect for a walk.")
+print(name + " remarked, \"It is so nice to see so many " + animal1 + "\nin the woods today! They really " + adjective2 + "-en the place up!\"")
